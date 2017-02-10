5597349cd44e48ca67244d0d83d76e99a765a6c0
dengrenjie <dengrenjie31@gmail.com>
2017-02-10 21:16:10 +0800

    add some css dynamic action

--- Exercise1/mypage.js
+++ Exercise1/mypage.js
@@ -1,4 +1,4 @@
-function showdiv(boxname) {
+function showdiv(boxname) {//show wechat code
         var element1=document.getElementById("blackbackground").style;
         var element2=document.getElementById(boxname).style;
         element1.display="block";
@@ -6,10 +6,14 @@ function showdiv(boxname) {
         document.body.style.overflow="hidden";
 }
 
-function hidediv() {
+function hidediv() {//hide wechat code
         var element1=document.getElementById("blackbackground").style;
         var element2=document.getElementById("wechatcontent").style;
         element1.display="none";
         element2.display="none";
         document.body.style.overflow="auto";
+}
+
+function favpage(){
+        window.open("http://ow.blizzard.cn");
 }
