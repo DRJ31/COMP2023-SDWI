5433465224a3576408a31193b1cc6917fc64dbbf
DRJ31 <dengrenjie31@gmail.com>
2017-02-26 11:30:57 +0800

    add regexp

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -23,6 +23,13 @@ window.onresize=function() {//change height of screen
         high.height="150px";
     }
 }
+function mainjudge(){//regexp
+    var strjudge=[];
+    strjudge[0]=/[a-z\^√!]/g;//judge if there are English letters in string
+    strjudge[1]=/\D/g;//judge if there are numbers in string
+    strjudge[2]=/[\+\-\*\/]$/g;//judge if + - * / is the last element of string
+    return strjudge;
+}
 function findscreen(){//get elements of calculator screen
     var arr=[];
     arr[0]=document.getElementById("above");
@@ -33,13 +40,23 @@ function findscreen(){//get elements of calculator screen
 function main(element) {//function of buttons
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    txt.innerHTML+=element;
+    var strjudge=mainjudge();
     var judge=element=="+"||element=="-"||element=="*"||element=="/";
+    if(txt.innerHTML.match(strjudge[0])==null){
+        txt.innerHTML+=element;
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null&&judge==true){
+        txt.innerHTML=txt1.innerHTML+element;
+    }
     if(judge==false){
         txt1.innerHTML=eval(txt.innerHTML);
     }
     else{
         txt1.innerHTML="";
+    }
+    if(txt1.innerHTML=="undefined"){
+        txt1.innerHTML=number[j-1];
+        console.log(number);
     }
     txt.style.height="90px";
     txt.style.color="black";
@@ -59,12 +76,17 @@ function shake(time){//your phone will shake if it is an android phone with chro
 function backspace(){//function of backspace button
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
+    var strjudge=mainjudge();
     var txtarr=txt.innerHTML.split("");
     txtarr.splice(txtarr.length-1,1);
     txt.innerHTML=txtarr.join("");
-    var element=txt.innerHTML;
-    var judge=element.length-element.lastIndexOf("+")==1||element.length-element.lastIndexOf("*")==1||element.length-element.lastIndexOf("-")==1||element.length-element.lastIndexOf("/")==1;
-    if(judge==false){
+    if(txt.innerHTML.match(strjudge[0])!=null){
+        txt1.innerHTML="";
+    }
+    else if(txt.innerHTML.length==0){
+        txt1.innerHTML="";
+    }
+    else if(txt.innerHTML.match(strjudge[2])==null){
         txt1.innerHTML=eval(txt.innerHTML);
     }
     else{
@@ -129,11 +151,14 @@ function larrow() {//left arrow function
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
     txt1.innerHTML="";
-    if(k>0){
+    if(number.length==0){
+        txt.innerHTML=="";
+    }
+    else if(k>0&&number[k].length>0){
         k--;
         txt.innerHTML=number[k];
     }
-    else{
+    else if(number[0].length>0){
         txt.innerHTML=number[0];
     }
     txt.style.height="90px";
@@ -148,11 +173,14 @@ function rarrow() {//right arrow function
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
     txt1.innerHTML="";
-    if(k<number.length-1){
+    if(txt.innerHTML==0){
+        txt.innerHTML="";
+    }
+    else if(k<number.length-1){
         k++;
         txt.innerHTML=number[k];
     }
-    else{
+    else if(number[0].length>0){
         txt.innerHTML=number[number.length-1];
     }
     txt.style.height="90px";
@@ -162,186 +190,276 @@ function rarrow() {//right arrow function
     txt1.style.fontSize="30px";
     txt1.style.color="#ccc";
 }
-
+//high class function begin from here
 function log() {
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.log(txt.innerHTML)/Math.log(10);
-    txt.innerHTML="log("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+    var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.log(txt.innerHTML) / Math.log(10);
+        txt.innerHTML = "log(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function sin() {
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.sin(txt.innerHTML);
-    txt.innerHTML="sin("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.sin(txt.innerHTML);
+        txt.innerHTML = "sin(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function cos() {
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.cos(txt.innerHTML);
-    txt.innerHTML="cos("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.cos(txt.innerHTML);
+        txt.innerHTML = "cos(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function tan() {
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.tan(txt.innerHTML);
-    txt.innerHTML="tan("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.tan(txt.innerHTML);
+        txt.innerHTML = "tan(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function exp() {//function of e^x
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.exp(txt.innerHTML);
-    txt.innerHTML="e^("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.exp(txt.innerHTML);
+        txt.innerHTML = "e^(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function xxx() {//x^2 function
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=txt.innerHTML*txt.innerHTML;
-    txt.innerHTML="("+content+")^2";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = txt.innerHTML * txt.innerHTML;
+        txt.innerHTML = "(" + content + ")^2";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function xxxx(){//x! function
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    var numbers=parseInt(txt.innerHTML);
-    var result=1;
-    while(numbers>1){
-        result=result*numbers*(--numbers);
-        numbers--;
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
     }
-    txt.innerHTML=content+"!";
-    number[j]=result;
-    txt1.innerHTML=result;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        var numbers = parseInt(txt.innerHTML);
+        var result = 1;
+        while (numbers > 1) {
+            result = result * numbers * (--numbers);
+            numbers--;
+        }
+        txt.innerHTML = content + "!";
+        number[j] = result;
+        txt1.innerHTML = result;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function log10() {//function of ln
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.log(txt.innerHTML);
-    txt.innerHTML="ln("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.log(txt.innerHTML);
+        txt.innerHTML = "ln(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function sqrt() {
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    txt1.innerHTML=Math.sqrt(txt.innerHTML);
-    txt.innerHTML="√("+content+")";
-    number[j]=txt1.innerHTML;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
+    }
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        txt1.innerHTML = Math.sqrt(txt.innerHTML);
+        txt.innerHTML = "√(" + content + ")";
+        number[j] = txt1.innerHTML;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
 
 function twox(){//function of 2^x
     var txt=findscreen()[0];
     var txt1=findscreen()[1];
-    var content=txt.innerHTML;
-    var num=txt.innerHTML;
-    var count=0;
-    var numbers=1;
-    while(count<num){
-        numbers*=2;
-        count++;
+        var strjudge=mainjudge();
+    if(txt.innerHTML.length==0){
+        alert("Please input numbers");
     }
-    txt1.innerHTML=numbers;
-    txt.innerHTML="2^("+content+")";
-    number[j]=numbers;
-    j++;
-    k=j;
-    txt.style.height="40px";
-    txt.style.color="#ccc";
-    txt.style.fontSize="30px";
-    txt1.style.height="90px";
-    txt1.style.fontSize="60px";
-    txt1.style.color="#78c357";
+    else if(txt.innerHTML.match(strjudge[0])!=null){
+        alert("Please reset and continue");
+    }
+    else {
+        var content = txt.innerHTML;
+        var num = txt.innerHTML;
+        var count = 0;
+        var numbers = 1;
+        while (count < num) {
+            numbers *= 2;
+            count++;
+        }
+        txt1.innerHTML = numbers;
+        txt.innerHTML = "2^(" + content + ")";
+        number[j] = numbers;
+        j++;
+        k = j;
+        txt.style.height = "40px";
+        txt.style.color = "#ccc";
+        txt.style.fontSize = "30px";
+        txt1.style.height = "90px";
+        txt1.style.fontSize = "60px";
+        txt1.style.color = "#78c357";
+    }
 }
