ec0d05469c7740a16c7b39a071d7f8739d427941
DRJ31 <dengrenjie31@gmail.com>
2017-03-12 20:25:54 +0800

    test new auto push shell

--- acp.sh
+++ acp.sh
@@ -2,25 +2,27 @@ echo "Do you want to push or just commit [p/c]: "
 read choice
 echo "Please write down your comments: "
 read comment
-function times {
-  for (( i = 5; i >= 0; i-- )); do
-    echo $i
-    sleep 1
-  done
+function confirm() {
+  echo "Are you sure that you are ready to $1 [Y/N]"
+  read yn
+  if [[ $yn == "y" || $yn == "Y" ]]; then
+    if [[ $1 == "commit" ]]; then
+      git commit -m "$comment"
+      git status
+    elif [[ $1 == "push" ]]; then
+      git push
+    fi
+  fi
 }
 if [ $choice == "p" ]
 then
   git add .
   git status
-  times
-  git commit -m "$comment"
-  git status
-  times
-  git push
+  confirm "commit"
+  confirm "push"
 elif [ $choice == "c" ]
 then
   git add .
   git status
-  times
-  git commit -m "$comment"
+  confirm "commit"
 fi
