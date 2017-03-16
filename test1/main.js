da81b932424b109337dfde274d88a3ee3555ae61
dengrenjie <dengrenjie31@gmail.com>
2017-03-16 11:06:33 +0800

    function done

---
+++ test1/main.js
@@ -0,0 +1,94 @@
+var years=[];//array of all years
+var regexp=/[^\D]\d/g;//find numbers in a string
+    for(var i=0;i<10;i++){
+        years[i]=[];
+        for(var j=0;j<10;j++){
+            var thisyear=2008+i-108+j*12;
+            years[i][j]=thisyear;
+        }
+    }
+    for(var k=10;k<12;k++){
+        years[k]=[];
+        for(var l=0;l<10;l++){
+            var thisyear=2006+k-10-108+l*12;
+            years[k][l]=thisyear;
+        }
+    }
+//get years of all
+function listyear(arrnum) {//list year in information page
+    var content="";
+    for(var i=0;i<years[arrnum].length;i++){
+        content+="<li>"+years[arrnum][i]+"</li>";
+    }
+    return content;
+}
+
+function srcimg(num){//find which img to src in information page
+    var imgsrc;
+    switch (num){
+        case 0:
+            imgsrc="img/zodiac-rat.png";
+            break;
+        case 1:
+            imgsrc="img/zodiac-ox.png";
+            break;
+        case 2:
+            imgsrc="img/zodiac-tiger.png";
+            break;
+        case 3:
+            imgsrc="img/zodiac-rabbit.png";
+            break;
+        case 4:
+            imgsrc="img/zodiac-dragon.png";
+            break;
+        case 5:
+            imgsrc="img/zodiac-snake.png";
+            break;
+        case 6:
+            imgsrc="img/zodiac-horse.png";
+            break;
+        case 7:
+            imgsrc="img/zodiac-sheep.png";
+            break;
+        case 8:
+            imgsrc="img/zodiac-monkey.png";
+            break;
+        case 9:
+            imgsrc="img/zodiac-rooster.png";
+            break;
+        case 10:
+            imgsrc="img/zodiac-dog.png";
+            break;
+        case 11:
+            imgsrc="img/zodiac-pig.png";
+            break;
+    }
+    return imgsrc;
+}
+
+function clickfun(num){//click img function
+    var src=srcimg(num);
+    var list=listyear(num);
+    var content='<img src='+src+' alt=""><ul>'+list+'</ul>';
+    document.getElementById("info").innerHTML=content;
+    document.getElementById("infocard").style.display="block";
+    document.getElementById("infocardbg").style.display="block";
+}
+
+function submitfun(){//submit button function
+    var number=document.getElementById("text").value;
+    for(var i=0;i<years.length;i++){
+        for(var j=0;j<years[i].length;j++){
+            if(years[i][j]==number){
+                var num=i;
+            }
+        }
+    }
+    clickfun(num);
+}
+
+function goback(){
+    document.getElementById("infocard").style.display="none";
+    document.getElementById("infocardbg").style.display="none";
+}
+
