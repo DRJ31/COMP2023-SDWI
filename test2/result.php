faf895be0cbbd9508c7a6c1cdcecbb7559d08dfd
dengrenjie <dengrenjie31@gmail.com>
2017-04-27 11:41:58 +0800

    add php in practical test 2

---
+++ test2/result.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://ols1alctg.qnssl.com/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <table class="table table-striped table-bordered">
+        <tr>
+            <td>ID</td>
+            <td><?php
+            echo $_POST["id"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><?php
+            echo $_POST["name"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><?php
+            echo $_POST["address"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Phone</td>
+            <td><?php
+            echo $_POST["phone"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Number of Adult</td>
+            <td><?php
+            echo $_POST["adult"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Number of Children</td>
+            <td><?php
+            echo $_POST["child"];
+                ?></td>
+        </tr>
+        <tr>
+            <td>Hobbies</td>
+            <td><?php
+            echo $_POST["hobby"];
+                ?></td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
