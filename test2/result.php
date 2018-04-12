0b8cb614f877b12cf72151ea251c9ad1556822d6
Renjie Deng <dengrenjie31@gmail.com>
2018-04-13 00:02:21 +0800

    Change cdn address again

--- test2/result.php
+++ test2/result.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Information</title>
-    <link rel="stylesheet" href="https://ols1alctg.qnssl.com/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.drjchn.com/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
